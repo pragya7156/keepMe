b3b77aab2be02c4cf081aaaa26d5f0f0f121f930
Pragya Singh <64683624+pragya7156@users.noreply.github.com>
2021-05-17 23:45:34 +0530

    url changes

--- client/src/assets/constants/BaseURL.js
+++ client/src/assets/constants/BaseURL.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = 'http://localhost:4000';
-// export const BACKEND_URL = 'https://diary07.herokuapp.com';
+// export const BACKEND_URL = 'http://localhost:4000';
+export const BACKEND_URL = 'https://diary-fullstack.herokuapp.com';
